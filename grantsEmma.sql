9d9832dcbbc9c35ca862ee82d44ee64a5227b3d1
Emmaloto <icyblaze16@gmail.com>
2017-04-23 15:39:12 -0500

    Testing Queries
    A file to test SQL Queries from PHP with permission granted from my database. Insert does NOT want to work, although deleting and displaying works fine. I believe it is a problem with the form, as it it not receiving the values from it. The login and grants refer to my database.

---
+++ grantsEmma.sql
@@ -0,0 +1,3 @@
+GRANT SELECT,DELETE,UPDATE,INSERT ON professorArchives.*  TO webuser@localhost             IDENTIFIED BY '9083ughf4';
+GRANT SELECT,DELETE,UPDATE,INSERT ON professorArchives.*  TO webuser@'%.sewanee.edu'       IDENTIFIED BY '9083ughf4';
+GRANT SELECT,DELETE,UPDATE,INSERT ON professorArchives.*  TO webuser@'%'                   IDENTIFIED BY '9083ughf4';
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php 
+
+  $host = "cook.sewanee.edu";
+  $user = "webuser";
+  $pass = "9083ughf4";
+  $db   = "professorArchives";
+
+?>
