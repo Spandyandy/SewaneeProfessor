a9f3b718b6919e9b0d0e22216989cd7c61d173b5
Spandyandy <kimj0@sewanee.edu>
2017-05-01 19:18:58 -0500

    Changes title of the page

--- profsByDept.php
+++ profsByDept.php
@@ -7,7 +7,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>sample</title>
+		<title>Sewanee Professor</title>
 		<meta charset = "utf-8" />
 		<meta name = "viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" />
